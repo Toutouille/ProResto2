a3cf76136d818b32b2bcfe0dae75db8b4d4e4d9e
petermarzinou <petermarzinou@hotmail.fr>
2015-12-21 23:58:35 +0100

    Add an animation to the restoActivity to swipe between restaurants. Layouts have been modified consequently.

--- app/src/main/java/com/example/tmary/proresto2/RestoActivity.java
+++ app/src/main/java/com/example/tmary/proresto2/RestoActivity.java
@@ -7,6 +7,7 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.PageTransformer;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -48,7 +49,7 @@ public class RestoActivity extends Activity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
+        mViewPager.setPageTransformer(true, new DepthPageTransformer());
 
         //On récupère les données du Bundle
         int pageSelected = 0;
@@ -143,9 +144,6 @@ public class RestoActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public String getIdRestoChoisi() {
-        return id_resto_choisi;
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -238,7 +236,7 @@ public class RestoActivity extends Activity {
                         +"\nSamedi : "+Resto.getMySchedule().getSaturday()
                         +"\nDimanche : "+Resto.getMySchedule().getSunday();
                 rootView = inflater.inflate(R.layout.fragment_resto, container, false);
-                imageView = (ImageView) rootView.findViewById(R.id.imageZone);
+                imageView = (ImageView) rootView.findViewById(R.id.imageResto);
                 section_label = (TextView) rootView.findViewById(R.id.section_label);
                 description = (TextView) rootView.findViewById(R.id.description);
                 schedule = (TextView) rootView.findViewById(R.id.schedule);
@@ -262,6 +260,124 @@ public class RestoActivity extends Activity {
 
                 return rootView;
 
+            }
+        }
+    }
+
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
+    public class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1);
+                view.setTranslationX(0);
+                view.setScaleX(1);
+                view.setScaleY(1);
+
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
+    public class CustomPageTransformer implements ViewPager.PageTransformer {
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            View imageView = view.findViewById(R.id.imageZone);
+            View contentView = view.findViewById(R.id.page_layout);
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left
+            } else if (position <= 0) { // [-1,0]
+                // This page is moving out to the left
+
+                // Counteract the default swipe
+                view.setTranslationX(pageWidth * -position);
+                if (contentView != null) {
+                    // But swipe the contentView
+                    contentView.setTranslationX(pageWidth * position);
+                }
+                if (imageView != null) {
+                    // Fade the image in
+                    imageView.setAlpha(1 + position);
+                }
+
+            } else if (position <= 1) { // (0,1]
+                // This page is moving in from the right
+
+                // Counteract the default swipe
+                view.setTranslationX(pageWidth * -position);
+                if (contentView != null) {
+                    // But swipe the contentView
+                    contentView.setTranslationX(pageWidth * position);
+                }
+                if (imageView != null) {
+                    // Fade the image out
+                    imageView.setAlpha(1 - position);
+                }
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right
             }
         }
     }
