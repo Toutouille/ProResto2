b231b9458e18236792c9e0ffb33c46a54d821ab8
petermarzinou <petermarzinou@hotmail.fr>
2016-01-13 13:03:31 +0100

    Modify the InfoActivity to use it with a fragment in order to have the fonctionnality of the return icon.

--- app/src/main/java/com/example/tmary/proresto2/InfoActivity.java
+++ app/src/main/java/com/example/tmary/proresto2/InfoActivity.java
@@ -1,16 +1,24 @@
 package com.example.tmary.proresto2;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
 import android.widget.RatingBar;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
@@ -18,43 +26,148 @@ import android.widget.Toast;
  */
 public class InfoActivity extends Activity {
 
-    private RatingBar ratingBar;
+    /**
+     * That will provide fragments for each of the sections. We use a
+     * FragmentPagerAdapter derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a FragmentStatePagerAdapter.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    /**
+     * That will host the section contents.
+     */
+    private static ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            public void onRatingChanged(RatingBar ratingBar, float rating,
-                                        boolean fromUser) {
-
-                Toast.makeText(InfoActivity.this, "Merci de votre avis !", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.containerInfo);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        setupActionBar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if(id == R.id.action_settings)
-        {
-            Toast.makeText(this, "Non implémenté", Toast.LENGTH_SHORT).show();
+    /**
+     * Set up the ActionBar
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        return super.onOptionsItemSelected(item);
     }
 
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(R.string.title_activity_info);
+
+        }
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_info";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            RatingBar ratingBar;
+            View rootView;
+            // Lock on portrait orientation
+            this.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            // Each page got his own menu
+            setHasOptionsMenu(true);
+            rootView = inflater.inflate(R.layout.fragment_info, container, false);
+
+            /*ratingBar = (RatingBar) rootView.findViewById(R.id.ratingBar);
+            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                public void onRatingChanged(RatingBar ratingBar, float rating,
+                                            boolean fromUser) {
+                    Toast.makeText(getActivity(), "Merci de votre avis !", Toast.LENGTH_SHORT).show();
+                }
+            });*/
+
+            return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.menu_info, menu);
+
+            super.onCreateOptionsMenu(menu, inflater);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+
+            int id = item.getItemId();
+            String str = Integer.toString(id);
+            Log.v("INFO", "onOptionsItemSelected"+str);
+            if (id == android.R.id.home) {
+                Toast.makeText(getActivity(), "Touché", Toast.LENGTH_SHORT);
+                startActivity(new Intent(getActivity(), Home.class));
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
